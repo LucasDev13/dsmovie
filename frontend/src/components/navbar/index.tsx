9c45766b9a712d04899c2b81365ee4abe055fb98
Lucas <lucaspontes140@gmail.com>
2022-01-11 22:01:00 -0300

    Criado o Navbar e modificado o pom.xml.

---
+++ frontend/src/components/navbar/index.tsx
@@ -0,0 +1,30 @@
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
+import { ReactComponent as LinkedIn } from 'assets/img/linkedin.svg';
+import './styles.css';
+
+function Navbar() {
+
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/LucasDev13">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link">/LucasDev13</p>
+                        </div>
+                    </a>
+                    {/* <a href='https://www.linkedin.com/in/pontes-lucas/'>
+                        <div className="dsmovie-contact-container">
+                            <LinkedIn />
+                            <p className="dsmovie-contact-link">/LinkedIn</p>
+                        </div>
+                    </a> */}
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
